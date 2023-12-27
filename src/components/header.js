2bbbe3d8209aa37408feb5abed9f23ffe4763bbb
Xoneris <kely42@gmx.ch>
2023-12-27 15:45:14 +0100

    added basic components

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+function Header (){
+    return (
+        <header>
+            <h2>This is the Header!</h2>
+        </header>
+    );
+}
+
+export default Header
---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+function Navigation (){
+    return (
+        <nav>
+            <ul>
+                <li>Nav Item 1</li>
+                <li>Nav Item 2</li>
+                <li>Nav Item 3</li>
+                <li>Nav Item 4</li>
+            </ul>    
+        </nav>
+    );
+}
+
+export default Navigation
