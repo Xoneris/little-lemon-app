fcdd0af797b09b5ad5e3f71c9e0ef556d2051187
Xoneris <kely42@gmx.ch>
2024-01-05 16:19:26 +0100

    Created a responsive Navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+
 
 function Navigation (){
+
+    const [mobileNavBar, setMobileBar] = useState(false);
+
     return (
         <nav>
-            <ul>
-                <li><Link to="/">Home</Link> </li>
-                
-                <li>About</li>
-                <li>Menu</li>
-                <li><Link to="/reservations">Reservations</Link></li>
-                <li>Order</li>
-                <li>Login</li>
-                <li><img src="./assets/icon_hamburger_menu.svg" /></li>
-            </ul>    
+            <div id={mobileNavBar === true ? "desktop-view-active" : "desktop-view-inactive"} >
+                <ul>
+                    <li><Link to="/">Home</Link> </li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li><Link to="/reservations">Reservations</Link></li>
+                    <li>Order</li>
+                    <li>Login</li>
+                    <li>{mobileNavBar == false ? "False" : "True"}</li>
+                </ul>  
+            </div>
+            <div id="mobile-view">
+                <i onClick={() => {setMobileBar(!mobileNavBar)}}>
+                    <img src="./assets/icon_hamburger_menu.svg" />
+                </i>
+            </div>  
         </nav>
-        
     );
 }
 
